0b10c244f12b297a88bcf405ab8c45feb187007b
nisbus <nisbus@gmail.com>
2014-02-14 16:15:09 +0000

    added centos dependencies

---
+++ manifests/dependencies/centos.pp
@@ -0,0 +1,12 @@
+# = Class to manage ubuntu and debian packages
+class nginx::dependencies::ubuntu {
+  if ! defined(Package['gcc']) { package { 'gcc': ensure => 'installed', provider => 'yum' } }
+  if ! defined(Package['gcc-c++']) { package { 'gcc-c++': ensure => 'installed', provider => 'yum' } }
+  if ! defined(Package['kernel-devel']) { package { 'kernel-devel': ensure => 'installed', provider => 'yum' } }
+  if ! defined(Package['readline-devel']) { package { 'readline-devel': ensure => 'installed', provider => 'yum' } }
+  if ! defined(Package['ncurses-devel']) { package { 'libncurses5-dev': ensure => 'installed', provider => 'yum' } }
+  if ! defined(Package['pcre']) {        package { 'pcre':        ensure => 'installed', provider => 'yum' } }
+  if ! defined(Package['pcre-devel']) {    package { 'pcre-devel':    ensure => 'installed', provider => 'yum' } }
+  if ! defined(Package['openssl-devel']) {      package { 'openssl-devel':      ensure => 'installed', provider => 'yum' } }
+  if ! defined(Package['perl']) {            package { 'perl':            ensure => 'installed', provider => 'yum' } }
+}
