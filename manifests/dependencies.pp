7117daa675f49ba9c7408b91a26318864b688526
nisbus <nisbus@gmail.com>
2014-02-14 16:26:05 +0000

    fixing the OS detection

--- manifests/dependencies.pp
+++ manifests/dependencies.pp
@@ -2,7 +2,7 @@
 class nginx::dependencies {
   case $::operatingsystem {
     Ubuntu,Debian: { require nginx::dependencies::ubuntu }
-	Centos,RedHat: { require nginx::dependencies::centos }
+	CentOS,RedHat: { require nginx::dependencies::centos }
     default: { notify { 'not-available': message => "This OS is not supported" } }
   }
 }
